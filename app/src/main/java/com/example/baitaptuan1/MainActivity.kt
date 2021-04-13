6458b86f6694c6c5d710f4da4bfbe4f7a30534bb
yunjedi <kingofdragonjacklong2009@gmail.com>
2021-04-13 12:16:47 +0700

    test

--- app/src/main/java/com/example/baitaptuan1/MainActivity.kt
+++ app/src/main/java/com/example/baitaptuan1/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.baitaptuan1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        Toast.makeTest(this,"success", Toast.LENGTH_LONG).show();
     }
 }
